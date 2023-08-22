cbe7d4ca8a30c3a68051874b5786ffeb4f9e2c36
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-08-22 13:33:44 +0530

    2P Approach Traprainwater

--- DSAUPGRAD/Sorting And Searching/Interview150.js
+++ DSAUPGRAD/Sorting And Searching/Interview150.js
@@ -236,3 +236,30 @@ var trap = function (heights) {
   }
   return trappedWater;
 };
+function trap(heights) {
+  let left = 0;
+  let right = heights.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+  let result = 0;
+
+  while (left < right) {
+    if (heights[left] < heights[right]) {
+      if (heights[left] > leftMax) {
+        leftMax = heights[left];
+      } else {
+        result += leftMax - heights[left];
+      }
+      left++;
+    } else {
+      if (heights[right] > rightMax) {
+        rightMax = heights[right];
+      } else {
+        result += rightMax - heights[right];
+      }
+      right--;
+    }
+  }
+
+  return result;
+}
