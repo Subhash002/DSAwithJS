cb1f0b4722c356a006dbef9089a126792312882e
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-09-03 19:06:07 +0530

    trie

--- DSAUPGRAD/Sorting And Searching/Interview150.js
+++ DSAUPGRAD/Sorting And Searching/Interview150.js
@@ -840,10 +840,5 @@ Trie.prototype.startsWith = function(prefix) {
     return node !== null;
 };
 
-/** 
- * Your Trie object will be instantiated and called as such:
- * var obj = new Trie()
- * obj.insert(word)
- * var param_2 = obj.search(word)
- * var param_3 = obj.startsWith(prefix)
- */
+
+
