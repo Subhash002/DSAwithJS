b09c6ba10a2ea3de22f0d93422204fafcf4543e5
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-09-03 18:23:27 +0530

    Trie

--- DSAUPGRAD/Sorting And Searching/Interview150.js
+++ DSAUPGRAD/Sorting And Searching/Interview150.js
@@ -786,3 +786,64 @@ var singleNumber = function (nums) {
   return single;
 };
 // __________________------------------------ BITWISE OPERATION FIND SINGLE NUMBER-------------- ________________
+// Trie
+
+var Trie = function() {
+    this.root = {};
+};
+
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let node = this.root;
+    for (const c of word) {
+        if (node[c] === undefined) node[c] = {};
+        node = node[c];
+    }
+    node.isWord = true;
+};
+
+
+
+/** 
+ * @param {string} word
+ * @return {string}
+ */
+Trie.prototype.traverse = function(word) {
+    let node = this.root;
+    for (const c of word) {
+        node = node[c];
+        if (node === null || node === undefined) return null;
+    }
+    return node;
+};
+
+
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let node = this.traverse(word);
+    return node !== null && node.isWord === true;
+};
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.traverse(prefix);
+    return node !== null;
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
